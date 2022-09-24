a65f0f548ce64536cc4ffd81f7238b58db49fd70
Nikita <pabear666@gmail.com>
2022-09-24 12:45:46 +0400

    GraphicEngine

---
+++ GasStation/GraphicEngine/Common/Area.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GasStation.GraphicEngine.Common
+{
+    public class Area
+    {
+        readonly Panel _panel;
+        public Square[,] Squares { get; }
+        public Size SquareSize { get; }
+
+        public Area(Panel panel, Size squareSize)
+        {
+            _panel = panel;
+            SquareSize = squareSize;
+
+            int squareWidthLength = _panel.Width / SquareSize.Width;
+            int squareHeightLength = _panel.Height / SquareSize.Height;
+            Squares = new Square[squareWidthLength, squareHeightLength];
+
+            for (int i = 0; i < squareWidthLength; i++)
+            {
+                for (int j = 0; j < squareHeightLength; j++)
+                {
+                    var square = new Square(new Point(i * SquareSize.Width, j * SquareSize.Height), SquareSize);
+                    _panel.Controls.Add(square.Control);
+                    Squares[i, j] = square;
+                    square.Control.BackColor = Color.Red;
+                }
+            }
+        }
+    }
+}
